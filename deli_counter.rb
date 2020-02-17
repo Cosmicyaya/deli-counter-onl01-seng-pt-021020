85943e86b6ff19cb8325c440450b44bd6a752af7
Ryan Hollins <ballbaroi@gmail.com>
2020-02-17 18:46:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,7 +14,7 @@ def line(x)
 end
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
-  puts "welcome, #{name}. You are number #{katz_deli.length} in line."
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 def now_serving(array)
   if array.empty?
